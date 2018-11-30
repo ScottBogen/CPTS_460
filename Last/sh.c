50490f3d163a6a2e7d5681c7dbe3a40ded325da8
Scott Bogen <bogenscott@gmail.com>
2018-11-29 18:14:30 -0800

    sh actually does one cmd only

--- Last/sh.c
+++ Last/sh.c
@@ -13,7 +13,8 @@ int main(int argc, char *argv[ ]) {
   int n;
   int in, out;
   int status;
-  int pid = getpid();
+  int shpid = getpid();
+  int pid;
   char tty[32];
   gettty(tty);
 
@@ -21,55 +22,65 @@ int main(int argc, char *argv[ ]) {
     sh theory
 
     read stdin
-    tokenizes it into cmd arg1 arg2 ...
+    stdin is dummy data right now
 
     fork a child
-    switch to it
-      child: exec("cmd arg1 arg2")
+    if fork returns 0, we are the child
+      exec("cat f1");
+    if fork returns something else, child is busy
+      wait for child to die
+      when child is dead, fork again
+        assuming fork works, exec("cat f1");
+
+
 
     later we can look for pipes and redirection
   */
 
+  int stdin = 0;
+
   while (1) {
-    prints("SCOTT SHELL\n\r");
-    printf("\n\r scsh #%d $ ", pid);
+    prints("------SCOTT SHELL------\n\r");
+    printf("\n\r scsh #%d $ ", getpid());
     gets(uinput);
-
-
-    /*
-    int i = 0;
-    while(strtok(uinput, buf, ' ', i++)) {
-      // buf now stores one of four things:
-      //    1. cmd
-      //    2.
-    }*/
 
     printf("\n\r your input: %s\n\r", uinput);
 
+
     child = fork();
+    //printf("child = %d\n\r", child);
     if (child) {
-      parent();   // child busy, wait for it to wake up
+      wait(&status);
     }
     else {
-      printf("SH forked child for exec\n\r");
+      // funny fork happens first, it goes FUNNY -> SCOTSH
+      //printf("SH funny forked child %d for exec\n\r", getpid());
+      //pause(1);
       exec("cat f1");
     }
+    wait(&status);
+
+    //printf("Now we are back at shell again, pid = %d, time to loop\n\r", getpid());
+    // when I take away while(1), it will print the above line after SCOTSH is called.
     memset(uinput, 0, 128);
   }
 }
 
+/*
 int parent() {
   int pid, status;
   pid = wait(&status);
   if (pid==child) {
     child = fork();
     if (child) {
-      //continue;
+
     }
     else {
-      printf("SH forked child for exec\n\r");
+      //printf("SCOTSH forked child %d for exec\n\r", getpid());
+      //pause(1);
       exec("cat f1");
       return 0;
     }
   }
 }
+*/ 
