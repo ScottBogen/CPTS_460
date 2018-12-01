9af26a13c8caab039debc6b230278bc8c704ecff
Scott Bogen <bogenscott@gmail.com>
2018-11-30 21:12:02 -0800

    cat and grep should be totally done, probably unless stdin pipes somewhere

--- Last/cat.c
+++ Last/cat.c
@@ -3,12 +3,18 @@
 char buf[1024];
 char buf2[1024];
 char string[128];
+char tty[32];
 
 int main(int argc, char *argv[ ]) {
 
   int n;
   int in, out;
   int i, j;
+
+  gettty(tty);
+
+  int outtty = open(tty, O_WRONLY);
+
 
   // STDIN
   if (argc == 1) {
@@ -26,73 +32,36 @@ int main(int argc, char *argv[ ]) {
 
   // main loop
   while(1) {
-    n = read(in, buf, 1024);      // read from input
+    n = read(in, buf, 1);      // read from input
     if (n < 1) { break; }         // if no data, break
+    string[i] = buf[0];
 
-    // (almost definitely) user is entering from keyboard
-    if (n == 1) {
-      //prints("cat from STDIN\n\r");
-      prints(buf);              // user can see the buttons they've pressed
-      buf2[i++] = buf[0];       // add char to buf2
+    if (!in) { prints(buf); }
 
+    if (string[i] == 10) {     // 10 == '\n';
+      string[++i] = '\r';        // 13 == '\r';
 
-      // upon pressing enter:
-      if (buf[0] == 13) {
-        if (last != 10 && last != 0 && last != 13) {
-          buf2[i++] = '\0';
-          prints("\n\r");
-          write(out, buf2, i);
-          prints("\n\r");
-          break;
-        }
-      }
-      last = buf[0];
+      write(out, string, i);
+      write(outtty, "\r", 1);     // to bring the shit back without filling up string
+
+      memset(string, 0, 128);
+      i = 0;
     }
 
+    // STDIN user presses "enter"
+    else if (string[i] == 13 && !in) {
+      string[i++] = 10;
+      string[i++] = 13;
+      write(outtty, "\r\n", 2);     // to bring the shit back without filling up string
+      write(out, string, i);
+      memset(string, 0, 128);
+      i = 0;
+    }
 
-    // reading from file
     else {
-      //prints("cat from FILE\n\r");
-      i = j = 0;
-      while (strtok(buf, string, '\n', i++)) {    // if we can tokenize
-        while (string[j++]);
-        write(out, string, j);
-        prints("\n\r");
-      }
+      i++;
     }
-
-    j = 0;
-    memset(buf, 0, 1024);
-    memset(string, 0, 128);
   }
-
-  /*
-  // from STDIN
-  else {
-    prints("cat from STDIN\n");
-    int i = 0;
-    while ((n = read(in, buf, 1)) == 1) {
-
-      //prints(buf);      // IDEA: make it so that n reads in multiple bytes, but if n==1, then it prints
-
-      if (buf[0] == '\n') {
-        buf2[i++] = buf[0];
-        buf2[i] = '\0';
-        write(out, buf2, i);
-        i = 0;
-        continue;
-      }
-      else if (buf[0] == 13) {
-        if (buf2[i-1] != 10 && buf2[i-1] != 0 && buf2[i-1] != 13) {
-          buf2[i] = '\0';
-          write(out, buf2, i);
-          break;
-        }
-      }
-
-      buf2[i++] = buf[0];
-    }
-  }*/
 
   close(in); close(out);
 }
--- Last/grep.c
+++ Last/grep.c
@@ -2,6 +2,7 @@
 #include "ucode.c"
 
 char buf[1024];
+char tty[32];
 
 int main(int argc, char *argv[ ]) {
 
@@ -12,6 +13,10 @@ int main(int argc, char *argv[ ]) {
   char* pattern;
   char line[128];
   char c;
+
+  gettty(tty);
+
+  int outtty = open(tty, O_WRONLY);
 
   if (argc < 2) {
     prints("Not enough args\n\r");
@@ -56,31 +61,33 @@ int main(int argc, char *argv[ ]) {
 
   i = j = k = 0;
   int to_print = 0;
+  memset(line, 0, 128);
   while (1) {
     n = read(in, buf, 1);      // read in from buf
     if (n < 1) { break; }         // leave if no more bytes
 
-    //printf("buf = %s\n\r", buf);
+    //printf("buf = %c\t, ascii = %d\n\r", buf[0], buf[0]);
 
     line[i] = buf[0];
 
+    //printf("line = %s\n\r", line);
 
     if (buf[0] == 10) {
-      prints("\r");
-      line[++i] = 13;
-      j = 0;
+      line[++i] = 13;       // NOTE: this actually (somehow) fixes the double print error
       if (to_print) {
-        write(out, line, 128);
+        write(out, line, i);
+        write(outtty, "\r", 1);     // to bring the shit back without filling up string
+
       }
       to_print = 0;
-      i = 0;
+      i = j = 0;
       memset(line, 0, 128);
     }
 
     else {
       if (line[i] == pattern[j]) {
         j++;
-        if (j == length) {
+        if (j == length && !to_print) {
           to_print = 1;
         }
       }
